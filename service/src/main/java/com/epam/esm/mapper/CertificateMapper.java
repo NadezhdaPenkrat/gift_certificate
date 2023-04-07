aa450147f7ffdb81aba6948e94e599921140ead4
Nadya Penkrat <nadya.penkrat@gmail.com>
2023-04-07 15:36:38 +0300

    implement GiftCertificate service mapper uses JdbcTemplate  RowMapper

---
+++ service/src/main/java/com/epam/esm/mapper/CertificateMapper.java
@@ -0,0 +1,44 @@
+package com.epam.esm.mapper;
+
+import com.epam.esm.entity.Certificate;
+import org.springframework.jdbc.core.RowMapper;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.time.ZoneOffset;
+import java.time.ZonedDateTime;
+/**
+ * The class that uses a JdbcTemplate  RowMapper
+ * to display the rows of the ResultSet
+ * for each row.
+ * Implementations of this interface
+ * do the actual work of mapping each row
+ * to a result object
+ * in an API for basic operations
+ * with the application
+ *
+ * @author Nadezhda Penkrat
+ */
+public class CertificateMapper implements RowMapper<Certificate> {
+
+    @Override
+    public Certificate mapRow(ResultSet rs, int rowNum) throws SQLException {
+
+        Certificate certificate = new Certificate();
+        certificate.setId(rs.getLong("id"));
+        certificate.setName(rs.getString("name"));
+        certificate.setDescription(rs.getString("description"));
+        certificate.setPrice(rs.getBigDecimal("price"));
+        certificate.setDuration(rs.getInt("duration"));
+        certificate.setCreateDate(ZonedDateTime.ofInstant
+                        (rs.getTimestamp("create_date").toInstant(), ZoneOffset.UTC)
+                .toLocalDateTime()
+        );
+        certificate.setUpdateDate(ZonedDateTime.ofInstant
+                        (rs.getTimestamp("last_update_date").toInstant(), ZoneOffset.UTC)
+                .toLocalDateTime()
+        );
+
+        return certificate;
+    }
+}
---
+++ service/src/main/java/com/epam/esm/mapper/TagMapper.java
@@ -0,0 +1,31 @@
+package com.epam.esm.mapper;
+
+import com.epam.esm.entity.Tag;
+import org.springframework.jdbc.core.RowMapper;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+/**
+ * The class that uses a JdbcTemplate  RowMapper
+ * to display the rows of the ResultSet
+ * for each row.
+ * Implementations of this interface
+ * do the actual work of mapping each row
+ * to a result object
+ * in an API for basic operations
+ * with the application
+ *
+ * @author Nadezhda Penkrat
+ */
+public class TagMapper implements RowMapper<Tag> {
+
+    @Override
+    public Tag mapRow(ResultSet rs, int rowNum) throws SQLException {
+
+        Tag tag = new Tag();
+        tag.setId(rs.getLong("id"));
+        tag.setName(rs.getString("name"));
+
+        return tag;
+    }
+}
